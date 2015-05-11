2b23785c8ed8bc8638cfdcd560c4cdcc9cbaddfb
joscalgom <joscalgom@gmail.com>
2015-05-11 16:50:05 +0200

    commit

--- TrabajoAII/src/TrabajoAII/urls.py
+++ TrabajoAII/src/TrabajoAII/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
-
+#abc
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'TrabajoAII.views.home', name='home'),
